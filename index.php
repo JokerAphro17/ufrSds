6f00a18b0da693b06bbf62fb42926224f1112c53
Ilboudo Souleymane <ilboudosouleymane4@gmail.com>
2022-05-10 01:19:32 +0000

    dkj

--- index.php
+++ index.php
@@ -4,10 +4,77 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link 
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="assets/bootstrap5/css/bootstrap.min.css">
+    <script src="assets/bootstrap5/js/bootstrap.min.js" defer></script>
     <title>Gestionnaire UFR/SDS </title>
 </head>
-<body>
-    
+<body class="">
+<section class="vh-100 inscription" >
+  <div class="container h-20">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-lg-12 col-xl-11">
+        <div class="card text-black carte" style="border-radius: 25px;">
+        <h1 class="card-title text-center">GESTIONNAIRE UFR/SDS</h1>
+
+          <div class="card-body p-md-5">
+            <div class="row justify-content-center">
+              <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">INSCRIPTION</p>
+
+                <form class="mx-1 mx-md-4">
+
+                  <div class="d-flex flex-row align-items-center ">
+                    <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="text" id="form3Example1c" class="form-control" />
+                      <label class="form-label" for="form3Example1c">Nom complet</label>
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center ">
+                    <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="email" id="form3Example3c" class="form-control" />
+                      <label class="form-label" for="form3Example3c">Email</label>
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center ">
+                    <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="password" id="form3Example4c" class="form-control" />
+                      <label class="form-label" for="form3Example4c">Mot de password</label>
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center ">
+                    <i class="fas fa-key fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="password" id="form3Example4cd" class="form-control" />
+                      <label class="form-label" for="form3Example4cd">Repeter le mot de passe</label>
+                    </div>
+                  </div>
+
+                  <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                    <button type="button" class="btn btn-primary btn-lg">S'inscrire</button>
+                  </div>
+
+                </form>
+
+              </div>
+              <!-- <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+
+                <img src="assets/img/login.png" alt="Login" class="img-fluid mx-auto d-block" />
+                  
+
+              </div> -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 </body>
 </html>
